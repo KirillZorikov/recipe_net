fe5c0946c02bbc7b42a01462a7f59be409fb9ed4
kirill <k.zorikov@yandex.ru>
2021-06-04 01:56:10 +0500

    add api_user urls

--- recipe_net/urls.py
+++ recipe_net/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from api.api_user import urls as user_urls
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
+    re_path('api/', include(user_urls))
 ]
 
 if settings.DEBUG:
