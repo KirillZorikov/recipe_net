c635501bb897a3cf68b2b246441cdebf46ab5d54
KirillZorikov <k.zorikov@yandex.ru>
2021-07-09 16:00:28 +0500

    update tests: add project_name to path

--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -1,5 +1,28 @@
 import pytest
 
+from rest_framework.test import APIClient
+from django.conf import settings
+
+class CustomAPIClient(APIClient):
+    ADDITIONAL_TO_PATH = f'/{settings.PROJECT_NAME}'
+
+    def get(self, path, *args, **kwargs):
+       return super().get(self.ADDITIONAL_TO_PATH + path, *args, **kwargs)
+
+    def post(self, path, *args, **kwargs):
+        return super().post(self.ADDITIONAL_TO_PATH + path, *args, **kwargs)
+
+    def put(self, path, *args, **kwargs):
+        return super().put(self.ADDITIONAL_TO_PATH + path, *args, **kwargs)
+
+    def patch(self, path, *args, **kwargs):
+        return super().patch(self.ADDITIONAL_TO_PATH + path, *args, **kwargs)
+
+    def delete(self, path, *args, **kwargs):
+        return super().delete(self.ADDITIONAL_TO_PATH + path, *args, **kwargs)
+
+
+
 
 @pytest.fixture
 def token(db_user, django_user_model):
@@ -16,25 +39,19 @@ def token(db_user, django_user_model):
 
 @pytest.fixture
 def client(token):
-    from rest_framework.test import APIClient
-
-    return APIClient()
+    return CustomAPIClient()
 
 
 @pytest.fixture
 def user_client(token):
-    from rest_framework.test import APIClient
-
-    client = APIClient()
+    client = CustomAPIClient()
     client.credentials(HTTP_AUTHORIZATION=f'Bearer {token["access_admin"]}')
     return client
 
 
 @pytest.fixture
 def not_admin_client(token):
-    from rest_framework.test import APIClient
-
-    client = APIClient()
+    client = CustomAPIClient()
     client.credentials(
         HTTP_AUTHORIZATION=f'Bearer {token["access_not_admin"]}'
     )
