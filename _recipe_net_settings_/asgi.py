d187a0004dfcfc891c2b5e7bd0ca7a58d9a949c4
KirillZorikov <k.zorikov@yandex.ru>
2021-07-11 17:30:50 +0500

    replace project's settings

--- _recipe_net_settings_/asgi.py
+++ _recipe_net_settings_/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_net.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_recipe_net_settings_.settings')
 
 application = get_asgi_application()
